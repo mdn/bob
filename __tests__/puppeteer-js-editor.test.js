3f1b6b4a990b5fbfcb06dd77056134aaf7c79bd1
István Kárász <ikarasz@outlook.com>
2020-05-21 17:29:32 +0200

    test(css/jseditor): fix broken tests

--- __tests__/puppeteer-js-editor.test.js
+++ __tests__/puppeteer-js-editor.test.js
@@ -1,15 +1,15 @@
 describe('JS Editor', () => {
-    beforeAll(async () => {
-        await page.goto('http://127.0.0.1:4444/pages/js/array-concat.html');
-    });
-
     it('renders expected output after clicking Run', async () => {
-        const expectedOutput = '> Array ["a", "b", "c", "d", "e", "f"]';
+        await page.goto('http://127.0.0.1:4444/pages/js/array-concat.html');
+        const expectedOutput = [
+            '> "First Log Output: " Array ["a", "b", "c", "d", "e", "f"]',
+            '> "Second Log Output: " Array ["a", "b", "c", "D", "E", "F", "G"]',
+        ].join(`\n`);
 
         await page.waitForSelector('#execute');
         await page.click('#execute');
 
-        let outputContent = await page.$eval('#output code', elem =>
+        let outputContent = await page.$eval('.output code', elem =>
             elem.innerText.trim()
         );
         await expect(outputContent).toBe(expectedOutput);
--- __tests__/puppeteer-tabbed-editor.test.js
+++ __tests__/puppeteer-tabbed-editor.test.js
@@ -1,28 +1,23 @@
 describe('Tabbed Editor', () => {
     describe('base test', () => {
         beforeAll(async () => {
-            await page.goto('http://127.0.0.1:4444/pages/tabbed/table.html');
+            await page.goto('http://127.0.0.1:4444/pages/tabbed/address.html');
         });
 
         it('loads the expected HTML into the output element', async () => {
             const expectedOutput =
-                '<style>.output{background-color:#fff;border:15px solid #eee;' +
+                '<style>' +
+                '.output{background-color:#fff;border:15px solid #eee;' +
                 'box-shadow:inset 5px 5px 10px rgba(0, 0, 0, .3), inset -5px -5px 10px rgba(0, 0, 0, .2);' +
-                'font-size:0.9rem;line-height:1.5;margin:24px 1em 0 0;overflow:scroll;' +
-                'padding:30px}</style>' +
-                '<style>table,' +
-                'td { border: 1px solid #333;}' +
-                'thead,tfoot { background-color: #333; color: #fff;}' +
-                '</style><div class="output" style="height: 76%;"><table> <thead> <tr> ' +
-                '<th colspan="2">The table header</th> </tr> </thead> ' +
-                '<tbody> <tr> ' +
-                '<td>The table body</td> ' +
-                '<td>with two columns</td> ' +
-                '</tr> </tbody> ' +
-                '<tfoot> ' +
-                '<tr> ' +
-                '<td colspan="2">The table footer</td> ' +
-                '</tr> </tfoot></table></div>';
+                'font-size:0.9rem;line-height:1.5;margin:24px 1em 0 0;' +
+                'overflow:scroll;padding:30px}</style>' +
+                '<style>' +
+                'address { font-variant-caps: small-caps;}a { font-variant: normal;}' +
+                '</style><div class=\"output\" style=\"height: 67%;\">' +
+                '<address> James Rockford<br> 2354 Pacific Coast Highway<br> ' +
+                'California<br> USA<br> +311-555-2368<br> Email: ' +
+                '<a href=\"mailto:j.rockford@example.com\">j.rockford@example.com</a><br>' +
+                '</address></div>';
 
             await page.waitForSelector('#output');
 
