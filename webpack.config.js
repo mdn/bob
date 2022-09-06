f3dd3c389250c35d4cd54dcc99e77c0b40193d46
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-09-06 09:38:57 -0700

    Commence Webpack migration (#867)

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    "editor-css": "./editor/js/editable-css.js",
+    "editor-js": "./editor/js/editable-js.js",
+    "editor-wat": "./editor/js/editable-wat.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "docs/js"),
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      path: require.resolve("path-browserify"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "async",
+    },
+  },
+};
