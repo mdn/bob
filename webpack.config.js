d1504059234db26c9e34be538fcd2a195c3c0a89
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-09-07 12:06:10 -0700

    Migrate to Webpack (part 2) (#871)
    * Use Prism NPM module
    
    * Use Clipboard NPM module
    
    * Move lib out of subfolder
    
    * Add license banner to Webpack files
    
    * Ensure Babel is used and prepare for Webpack CSS loading
    
    * Run Prettier
    
    * Retain existing clipboard/prism versions
    
    * Fix Prism version

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
+  plugins: [
+    new webpack.BannerPlugin(
+      "mdn-bob (Builder of Bits) - © Mozilla Corporation, MIT license"
+    ),
+  ],
   entry: {
     "editor-css": "./editor/js/editable-css.js",
     "editor-js": "./editor/js/editable-js.js",
     "editor-wat": "./editor/js/editable-wat.js",
+    "css-examples-libs": "./editor/js/css-examples-libs.js",
   },
   output: {
     path: path.resolve(__dirname, "docs/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
     fallback: {
