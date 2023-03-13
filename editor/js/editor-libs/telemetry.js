67c81fba4c99984d35ed7aa36f030ae4f37fb260
Claas Augner <caugner@mozilla.com>
2023-03-13 17:02:16 +0100

    chore(telemetry): add documentation

--- editor/js/editor-libs/telemetry.js
+++ editor/js/editor-libs/telemetry.js
@@ -2,6 +2,12 @@ import { postParentMessage } from "./events.js";
 
 const ACTION_COUNTS_KEY = "action-counts";
 
+/**
+ * Reads action counts from local storage.
+ * Ignores action counts that belong to another example.
+ *
+ * @returns The action counts from the previous session, if available.
+ */
 function readActionsCounts() {
   try {
     const value = JSON.parse(localStorage.getItem(ACTION_COUNTS_KEY));
@@ -15,6 +21,11 @@ function readActionsCounts() {
   return {};
 }
 
+/**
+ * Writes action counts to local storage, to persist them between reloads.
+ *
+ * @param {object} counts - The current action counts.
+ */
 function persistActionCounts(counts) {
   try {
     localStorage.setItem(
@@ -29,12 +40,23 @@ function persistActionCounts(counts) {
   }
 }
 
+/**
+ * Action counts by key.
+ * Used to distinguish 1st/2nd/etc occurrences of the same event.
+ */
 const actionCounts = readActionsCounts();
+
+/**
+ * Last observed action.
+ * Used to ignore multiple input actions until another action happened.
+ */
 let lastAction = null;
 
 /**
- * @param {string} key
- * @param {boolean} once
+ * Records an action, by counting it and forwarding it to the window parent.
+ *
+ * @param {string} key - Distinct name of the type of action.
+ * @param {boolean} deduplicate - Should multiple actions of the same type be ignored until another action occurred?
  */
 export function recordAction(key, deduplicate = false) {
   actionCounts[key] = actionCounts[key] ?? 0;
@@ -53,12 +75,15 @@ export function recordAction(key, deduplicate = false) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  // User focuses the iframe.
   window.addEventListener("focus", () => recordAction("focus"));
 
+  // User copies / cuts / paste text in the iframe.
   window.addEventListener("copy", () => recordAction("copy"));
   window.addEventListener("cut", () => recordAction("cut"));
   window.addEventListener("paste", () => recordAction("paste"));
 
+  // User clicks on any element with an id.
   window.addEventListener("click", (event) => {
     const id = event.target.id;
     if (id) {
