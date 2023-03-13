5503cf4db85fbfb346fba2fc34fd8b5c1fa4dafc
Claas Augner <caugner@mozilla.com>
2023-03-13 15:38:07 +0100

    feat(editor): persist action counts between reloads
    Allows to count multiple resets.

--- editor/js/editor-libs/events.js
+++ editor/js/editor-libs/events.js
@@ -55,26 +55,49 @@ function postParentMessage(type, values) {
   parent?.postMessage({ type, url: window.location.href, ...values }, "*");
 }
 
-const actionCounts = {};
+const ACTION_COUNTS_KEY = "action-counts";
+
+function readActionsCounts() {
+  try {
+    const value = JSON.parse(localStorage.getItem(ACTION_COUNTS_KEY));
+    window.console.log("readActionsCount", value);
+    if (value && value.href === window.location.href && value.counts) {
+      return value.counts;
+    }
+  } catch (e) {
+    window.console.warn("Unable to read action counts from localStorage", e);
+  }
+
+  return {};
+}
+
+function persistActionCounts(counts) {
+  try {
+    localStorage.setItem(
+      ACTION_COUNTS_KEY,
+      JSON.stringify({
+        href: window.location.href,
+        counts,
+      })
+    );
+  } catch (e) {
+    window.console.warn("Unable to write action counts to localStorage", e);
+  }
+}
+
+const actionCounts = readActionsCounts();
+
 /**
  * @param {string} key
  * @param {boolean} once
  */
-export function postActionMessage(key, once = false) {
+export function postActionMessage(key) {
   actionCounts[key] = actionCounts[key] ?? 0;
-  let source = key;
-
-  if (once && actionCounts[key] > 0) {
-    return;
-  }
-
-  if (!once) {
-    source += ` -> ${actionCounts[key]}`;
-  }
-
-  postParentMessage("action", { source });
-
+  let source = `${key} -> ${actionCounts[key]}`;
   actionCounts[key]++;
+
+  persistActionCounts(actionCounts);
+  postParentMessage("action", { source });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -87,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("click", (event) => {
     const id = event.target.id;
     if (id) {
-      postActionMessage(`click@${id}`, id === "reset");
+      postActionMessage(`click@${id}`);
     }
   });
 
