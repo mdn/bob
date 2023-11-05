efff012fc70d4fb0c4b07033e67f36b5b4bfa10e
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-11-04 21:23:37 -0700

    Leave webpack configuration as pure JavaScript
    Converting the Webpack config to TypeScript causes more issues than it solves.  Additionally, the Webpack documentation doesn't mention anything about using a .ts file for the configuration in its TypeScript guide.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath } from "node:url";
 import { createRequire } from "node:module";
 import path from "node:path";
-import webpack, { Configuration } from "webpack";
+import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 const require = createRequire(import.meta.url);
 
-const config: Configuration = {
+const config = {
   mode: "production",
   plugins: [
     new webpack.BannerPlugin(
@@ -61,10 +61,7 @@ const config: Configuration = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: (
-                resourcePath: string,
-                rootContext: string,
-              ): string => {
+              publicPath: (resourcePath, rootContext) => {
                 // publicPath is the relative path of the resource to the context
                 // e.g. for ./css/admin/main.css the publicPath will be ../../
                 // while for ./css/main.css the publicPath will be ../
